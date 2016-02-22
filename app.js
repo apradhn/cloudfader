1ba875dd0376a2d541444bafc0b62ccea4a5fe56
apradhn <abhijit.pradhan6@gmail.com>
2016-02-21 22:32:56 -0500

    Changes port number

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.listen(3000, function() {
-	console.log('CloudFader app listening on port 3000!');
+app.listen(process.env.PORT || 5000, function() {
+	console.log('CloudFader app started!');
 });
